44752cc2ab273ca4271e30e6bcd801c3fa14e5b4
BingWang0627 <wangbing.imail@gmail.com>
2015-07-07 23:22:59 -0400

    Update main.js
    updated js file to automatically load products to the html.

--- js/main.js
+++ js/main.js
@@ -23,35 +23,35 @@ var jeans = new Product ('Jeans', 29.99, , 'Men\'s and Women\'s shoes');
 var prodList=[
 	{"name": "Polo Shirt",
 	 "price": "$24.99",
-	 "pic": "../img/poloShirt.jpg",
-	 "description:"Men's Polo Shirt"
+	 "pic": "'img/poloShirt.jpg'",
+	 "description":"Men&#39s Polo Shirt"
 	}, 
 	{"name": "Sundress",
 	 "price": "$29.99",
-	 "pic": "../img/sundress.jpg"
-	 "description":"Ladies' Sundress"
+	 "pic": "'img/sundress.jpg'",
+	 "description":"Ladies&#39 Sundress"
 	},
 	{"name": "Shoes",
 	 "price": "$74.99",
-	 "pic": "../img/shoes.jpg"
-	 "description":"Men's and Women's Shoes"
+	 "pic": "'img/shoes.jpg'",
+	 "description":"Men&#39s and Women&#39s Shoes"
 	},
 	{"name": "Blouse",
 	 "price": "$18.99",
-	 "pic": "../img/shoes.jpg"
-	 "description":"Women's Blouse"
+	 "pic": "'img/shoes.jpg'",
+	 "description":"Women&#39s Blouse"
 	},
 	{"name": "Jeans",
 	 "price": "$94.99",
-	 "pic": "../img/jeans.jpg"
-	 "description":"Men's and Women's Jeans"
+	 "pic": "'img/jeans.jpg'",
+	 "description":"Men&#39s and Women&#39s Jeans"
 	}
 ]
 
-$(document).ready(function(e){
+$(document).ready(function(){
 	//e.preventDefault();
 	(prodList.forEach(function(ele, index){
-		$("#prod").append("<div class='col-sm-4 col-lg-4 col-md-4'><div class="thumbnail"><img src="+ele.pic+"><div class="caption"><h4 class="pull-right">"+ele.price+"</h4><h4>"+ele.name+"</h4></div></div></div>");
+		$("#prod").append("<div class='col-sm-4 col-lg-4 col-md-4'><div class='thumbnail'><img src="+ele.pic+"><div class='caption'><h4 class='pull-right'>"+ele.price+"</h4><h4>"+ele.name+"</h4></div></div></div>");
 	}));
 });
 
